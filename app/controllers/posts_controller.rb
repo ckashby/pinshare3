7c8a477f6c1a2c4e227154fd5e08aed36b270c68
ckashby <clay.ashby@gmail.com>
2018-08-28 09:20:16 -0700

    Created minimal posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,39 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  def update
+  end
+
+  def destroy
+  end 
+
+    private
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :image, :user_id)
+    end
 end
