fd221e9f70ee41db76b4d667a007986072a65f61
ckashby <clay.ashby@gmail.com>
2018-08-29 09:10:25 -0700

    Cleaned up forms and added, html: { multipart: true }

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
-  validates_presence_of :title 
+  validates :title, presence: true, length: {minimum: 2}
+  validates :image, file_size: { less_than: 1.megabytes }
 end
